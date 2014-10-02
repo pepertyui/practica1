6e22933aab82485f8d6b206ce81ba8ba7793a3ad
pepertyui <pepe_davila@live.com.mx>
2014-10-02 13:21:22 -0500

    inicio del programa

---
+++ practica1p/Program.cs
@@ -0,0 +1,25 @@
+/*
+ * Creado por SharpDevelop.
+ * Usuario: BETY
+ * Fecha: 02/10/2014
+ * Hora: 01:17 p.m.
+ * 
+ * Para cambiar esta plantilla use Herramientas | Opciones | Codificación | Editar Encabezados Estándar
+ */
+using System;
+
+namespace practica1p
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			
+			// TODO: Implement Functionality Here
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
